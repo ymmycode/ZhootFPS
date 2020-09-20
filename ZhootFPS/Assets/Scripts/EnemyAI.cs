eac449c3688156a0699c04889cbb75b0d285a65f
Achmat Fauzi <achmatfauzi86@gmail.com>
2020-09-21 04:12:16 +0700

    Adding Unity Standard Assets, Using FirstPersonController Prefab, Added Enemy AI, and Enemy Targeting Mechanics

---
+++ ZhootFPS/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 10f;
+    [SerializeField] bool isProvoked = false;
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (isProvoked == true)
+        {
+            EngageTarget();
+        }
+        else if( distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if(distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if(distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }   
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log("Youve been attacked");
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
